f0b09b535335647eda0f35315f43c7362dab34fa
Kingshlin Vershis <118352156+KingslinVershis@users.noreply.github.com>
2024-08-22 11:42:56 +0530

    Time: 1 ms (89.97%), Space: 45.7 MB (42.24%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       int buy_price = prices[0];
+       int sell_price =0;
+       int max = 0; 
+       for(int i=1;i<prices.length;i++){
+           if(buy_price>prices[i]){
+               buy_price = prices[i];
+           }
+           else{
+               sell_price = prices[i] - buy_price;
+               buy_price = prices[i];
+               max += sell_price; 
+           }
+       }
+        return max; 
+    }
+}
